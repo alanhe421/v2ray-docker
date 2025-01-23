8175edaabae5178faa01fb688a536c830b0b018c
Alan He <qianghe421@gmail.com>
2025-01-23 15:05:36 +0800

    chore: enhance init-v2ray.sh for conditional installation of Docker and Docker Compose
    - Added checks to install Docker and Docker Compose only if they are not already installed.
    - Updated .gitignore to include .DS_Store.

--- v2ray_config/init-v2ray.sh
+++ v2ray_config/init-v2ray.sh
@@ -4,15 +4,25 @@ if ! [ -x "$(command -v wget)" ]; then
   yum -y install wget
 fi
 
-# install docker
-wget -qO- https://get.docker.com/ | sh
-sudo systemctl enable docker
-sudo systemctl start docker
+# install docker if not exists
+if ! [ -x "$(command -v docker)" ]; then
+  echo "Installing docker..."
+  wget -qO- https://get.docker.com/ | sh
+  sudo systemctl enable docker
+  sudo systemctl start docker
+else
+  echo "Docker is already installed"
+fi
 
-# install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# install docker-compose if not exists
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo "Installing docker-compose..."
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+else
+  echo "Docker-compose is already installed"
+fi
 
 # timezone
 timedatectl set-timezone Asia/Shanghai
